3e508a79554734812dcb81b740b39ba668b8d49e
Zufiya <idrisizufiya@gmail.com>
2023-10-14 15:22:15 +0530

    Added routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import About from './Components/About';
-// import Blogs from './Components/Blogs';
-// import Contacts from './Components/Contacts';
-// import Profile from './Components/Profile';
-// import Resume from './Components/Resume';
-// import Works from './Components/Works';
-// import Error from './Components/Error';
-
-// const App = () => {
-//   return (
-//     <Router>
-
-//       <Routes>
-//         <Route path="/" element={<About />} />
-//         <Route path="/blogs" element={<Blogs />} />
-//         <Route path="/contacts" element={<Contacts />} />
-//         <Route path="/profile" element={<Profile />} />
-//         <Route path="/resume" element={<Resume />} />
-//         <Route path="/works" element={<Works />} />
-//         <Route path="*" element={<Error />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import About from './Components/About';
