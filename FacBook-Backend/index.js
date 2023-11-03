f2ddb521fe3ca39eecb348e84fb3bb239c3bf7f3
Zufiya Idrisi <idrisizufiya@gmail.com>
2023-11-03 21:33:40 +0530

    MP2A project integration

--- FacBook-Backend/index.js
+++ FacBook-Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
